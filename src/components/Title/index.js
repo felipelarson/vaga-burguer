92f44d0f7e7fa8486538fb45dcc33ffcd0fed77f
Felipe Silveira <felipelarson@msn.com>
2022-03-16 11:01:14 +0000

    chore: position components

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 export const Title = () => {
     return (
-        <div>
+        <div className='title'>
             <h4 className='top-title'>OPEN TODAY FOR GREAT FOOD</h4>
             <h1 className='main-title'>You Can Make<br /> Your Own Burger</h1>
             <h4 className='subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor incididunt ut labore et dolore</h4>
