a0815956e60e19bd3f93f68d07d6084da9424a09
Felipe Silveira <felipelarson@msn.com>
2022-03-16 10:26:00 +0000

    chore: styled component button hamburguer

--- src/components/ButtonHamburguer/index.js
+++ src/components/ButtonHamburguer/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MenuMobile } from '../MenuMobile'
 import BtnHamburguer from './../../assets/img/ButtonHamburguer.png'
 import "./styles.css"
 
 export const ButtonHamburguer = () => {
+
+    const [open, setOpen] = useState(false)
+
+    const handleClickOpen = () => setOpen(!open)
+
     return (
-        <button className='btnBurguer'>
-            <img src={BtnHamburguer} alt='btnHamburguer' />
-        </button>
+        <>
+            <button className='btnBurguer' onClick={handleClickOpen}>
+                <img src={BtnHamburguer} alt='btnHamburguer' />
+            </button>
+            {open && <MenuMobile />}
+        </>
     )
 }
