72847b7f5d564757de13fd3586202c3194e6ffee
Felipe Silveira <felipelarson@msn.com>
2022-03-16 10:26:22 +0000

    feat: created menu mobile

---
+++ src/components/MenuMobile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.css"
+
+export const MenuMobile = () => {
+    return (
+        <ul className='menu-mobile'>
+            <li>Home</li>
+            <li>New Menu</li>
+            <li>About Us</li>
+            <li>Main Course</li>
+        </ul>
+    )
+}
