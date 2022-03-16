a4f68ea7b81f30915f1790e5a15c73bc31c9069e
Felipe Silveira <felipelarson@msn.com>
2022-03-16 10:49:29 +0000

    chore: styled components

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './styles.css'
 
 export const Title = () => {
     return (
         <div>
-            <h4>OPEN TODAY FOR GREAT FOOD</h4>
-            <h2>You Can Make Your Own Burger</h2>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do #1</h4>
-            <button>GET START</button>
+            <h4 className='top-title'>OPEN TODAY FOR GREAT FOOD</h4>
+            <h1 className='main-title'>You Can Make<br /> Your Own Burger</h1>
+            <h4 className='subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod tempor incididunt ut labore et dolore</h4>
+            <button className='button-start'>GET STARTED</button>
         </div>
     )
 }
