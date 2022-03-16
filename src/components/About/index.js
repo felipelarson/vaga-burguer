8748ae179f4ce38db8061d5e78a628076955d0fd
Felipe Silveira <felipelarson@msn.com>
2022-03-16 10:25:45 +0000

    chore: styled component about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 export const About = () => {
     return (
         <div>
-            <h2>About Us</h2>
+            <h2 className="about">About Us</h2>
             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</h5>
-            <div className="rectangle"></div>
         </div>
     )
 }
